974397719a992bf4fc21f1e55cf0e83f1ebe018e
Grant Montgomery <lettucebowler@gmail.com>
2022-07-16 02:59:17 -0500

    bleh 7

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,4 @@
-export async function get() {
+export async function GET() {
 	const pokemonResponse = await fetch(`https://pokeapi.co/api/v2/pokemon-species?limit=905`);
 	const pokemon = await pokemonResponse.json();
 	const { results } = pokemon;
