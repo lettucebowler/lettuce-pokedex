0f01bc4799899a300a4b3ad00c47614238826b95
Grant Montgomery <lettucebowler@gmail.com>
2022-09-18 01:48:35 -0500

    fix padding on layout

--- src/routes/species/[species]/variant/[variant]/form/[form]/+layout.svelte
+++ src/routes/species/[species]/variant/[variant]/form/[form]/+layout.svelte
@@ -1,11 +1,10 @@
 <script lang="ts">
 	import SpeciesNav from '$lib/components/SpeciesNav.svelte';
 	import TypeBorder from '$lib/components/TypeBorder.svelte';
-
 	export let data: import('./$types').LayoutData;
 </script>
 
-<div class="w-full max-w-[1200px] m-auto flex flex-col gap-2 pl-1 pr-1 pb-1">
+<div class="w-full max-w-[1200px] m-auto flex flex-col gap-2">
 	<TypeBorder types={['default']}>
 		{#if data.navigation}
 			<SpeciesNav navData={data.navigation} />
