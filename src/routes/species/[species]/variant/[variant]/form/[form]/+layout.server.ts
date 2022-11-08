91aa386518edb38931c15a6595b8fe757a0957eb
Grant Montgomery <lettucebowler@gmail.com>
2022-11-08 13:42:09 -0600

    csr: false

--- src/routes/species/[species]/variant/[variant]/form/[form]/+layout.server.ts
+++ src/routes/species/[species]/variant/[variant]/form/[form]/+layout.server.ts
@@ -1,5 +1,7 @@
 import { getNavEntries } from '$lib/util/pokemonData';
 
+export const csr = false;
+
 export const load: import('./$types').PageServerLoad = async ({ params }) => {
 	return {
 		navigation: getNavEntries(params?.species || '')
